6bc35571c846ef87120e50554d3f9af834b87a59
cyril poly <cyrilpoly,lcs24@jecc.ac.in>
2026-01-22 21:55:31 +0530

    changes

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="123",
+        password="edun",
         database="project"
     )
 
