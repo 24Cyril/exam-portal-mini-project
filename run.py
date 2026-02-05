be78e78ac4cc79835617900d4b8b7d9ce815ae93
Soundharya-Gangadharan <soundharyagangadharan@gmail.com>
2026-02-05 10:43:09 +0530

    student profile updation

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def get_db_connection():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="Jec@023",
+        password="123",
         database="project"
     )
 
